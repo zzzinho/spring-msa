ef8e55d93b186db6d5f3421451b088ff3a5a541b
jinho <hahoh0013@gmail.com>
2021-10-28 17:00:50 +0900

    resource

--- licensing-service/src/main/java/com/example/licensingservice/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/example/licensingservice/LicensingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.licensingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
