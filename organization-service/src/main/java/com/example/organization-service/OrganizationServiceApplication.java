307d741cc9ba371e4f1a2716957b1185c4c4226b
jinho <hahoh0013@gmail.com>
2021-11-02 13:10:57 +0900

    organization service port

--- organization-service/src/main/java/com/example/organization-service/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/example/organization-service/OrganizationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrganizationApplication {
+public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrganizationApplication.class, args);
+		SpringApplication.run(OrganizationServiceApplication.class, args);
 	}
 
 }
