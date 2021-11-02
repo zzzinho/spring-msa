0d90e39c854197b49f42466b1f5f3631fb8d57c7
jinho <hahoh0013@gmail.com>
2021-11-02 13:44:56 +0900

    organization port

---
+++ organization-service/src/main/java/com/example/organizationservice/model/Organization.java
@@ -0,0 +1,27 @@
+package com.example.organizationservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="organizations")
+public class Organization {
+    @Id
+    @Column(name = "organization_id", nullable = false)
+    private String id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "contact_name", nullable = false)
+    private String contactName;
+    @Column(name = "contact_email", nullable = false)
+    private String contactEmail;
+    @Column(name = "contact_phone", nullable = false)
+    private String contactPhone;
+}
